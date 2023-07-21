b2b2f395528bb7803b3eae6a5c45f90503f3b4e1
tatsuyayoshida <yoshida@nawgraph.com>
2023-07-21 17:31:10 +0900

    up

---
+++ assets/js/slide-func.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('.slides').slick({
+  centerMode: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  lazyLoad: 'ondemand',
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+});
