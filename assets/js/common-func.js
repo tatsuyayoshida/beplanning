fb7afbe4bdf89553406f970c772b1920bfc1f064
tatsuyayoshida <yoshida@nawgraph.com>
2023-08-17 10:06:40 +0900

    update

--- assets/js/common-func.js
+++ assets/js/common-func.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
   $('.slides').slick({
   centerMode: true,
   autoplay: true,
@@ -28,5 +28,25 @@ $(document).ready(function(){
     }
   ]
 });
+
+$('a[href^="#"]').click(function(){
+    var adjust = 0;
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+
+  /*inview*/
+  $('.inview').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('on');
+    } else {
+
+    }
+  });
+
 
 });
